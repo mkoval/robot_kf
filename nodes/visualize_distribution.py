131c851bdc374ba4a8370fea17e9b63c59ccb7ff
Michael Koval <koval.michael@gmail.com>
2012-05-29 20:16:35 -0400

    distribution visualization works

--- nodes/visualize_distribution.py
+++ nodes/visualize_distribution.py
@@ -7,26 +7,30 @@ import numpy as np
 from std_msgs.msg import Header
 from geometry_msgs.msg import Point32
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import create_cloud, PointCloud
+from sensor_msgs.msg import PointCloud
 
 num_samples = 100
 
 def callback(odom):
     position = odom.pose.pose.position
 
-    mean = np.array([ position.x, position.y, position.z ])
-    cov = np.array(odom.pose.covariance)
-    np.reshape(cov, (6, 6))
+    mean = np.array([ position.x, position.y ])
+    cov_raw = np.array(odom.pose.covariance)
+    cov_raw = np.reshape(cov_raw, (6, 6))
+    cov = cov_raw[0:2, 0:2]
     samples = np.random.multivariate_normal(mean, cov, num_samples)
 
-    print samples
+    pub_cloud.publish(PointCloud(
+        header = odom.header,
+        points = [ Point32(x, y, position.z) for (x, y) in samples ]
+    ))
 
 def main():
     global pub_cloud
 
     rospy.init_node('visualize_distribution', anonymous=True)
     sub_odom  = rospy.Subscriber('odom_fused', Odometry, callback)
-    pub_cloud = rospy.Publisher('odom_distribution', PointCloud2)
+    pub_cloud = rospy.Publisher('odom_distribution', PointCloud)
 
     rospy.spin()
 
